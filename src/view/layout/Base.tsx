b56a95dfc4dfa9329a60eeab6f5ad898177a3d04
nemo-omen <nemo.omen@gmail.com>
2024-01-25 11:50:05 -0600

    Update page title

--- src/view/layout/Base.tsx
+++ src/view/layout/Base.tsx
@@ -10,7 +10,7 @@ export const Base = ({children, title}) => {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="/public/css/main.css" />
-      <title>Stringer{title}</title>
+      <title>Current{title}</title>
     </head>
     <body>
       <Header />
